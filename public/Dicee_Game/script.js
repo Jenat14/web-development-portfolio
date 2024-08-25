2683acd39964a6864626093af877f56f2a790276
unknown <jenatcanil14@gmail.com>
2024-08-26 00:21:40 +0530

    Links

--- public/Dicee_Game/script.js
+++ public/Dicee_Game/script.js
@@ -1,11 +1,11 @@
 var randomNum1 = Math.random();
 randomNum1 =Math.round((randomNum1*5)+1);
-var dice1 = "Images/dice"+randomNum1+".png";
+var dice1 = "images/dice"+randomNum1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",dice1)
 
 var randomNum2 = Math.random();
 randomNum2 =Math.round((randomNum2*5)+1);
-var dice2 = "Images/dice"+randomNum2+".png";
+var dice2 = "images/dice"+randomNum2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",dice2)
 
 if(randomNum1 > randomNum2){
