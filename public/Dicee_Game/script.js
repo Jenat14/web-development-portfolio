27d155e66c286e111afaf09e95cf941c2738d77c
unknown <jenatcanil14@gmail.com>
2024-08-26 00:13:46 +0530

    Added Project

--- public/Dicee_Game/script.js
+++ public/Dicee_Game/script.js
@@ -9,14 +9,14 @@ var dice2 = "Images/dice"+randomNum2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",dice2)
 
 if(randomNum1 > randomNum2){
-    document.querySelector("h1").textContent="Player 1 is the Winner! 🥳";
+    document.querySelector("h1").innerHTML="Player 1 is the Winner! 🥳";
     document.querySelector("h1").classList.add("smaller")
 }
 else if(randomNum1 < randomNum2){
-    document.querySelector("h1").textContent="Player 2 is the Winner! 🥳";
+    document.querySelector("h1").innerHTML="Player 2 is the Winner! 🥳";
     document.querySelector("h1").classList.add("smaller")
 }
 else{
-    document.querySelector("h1").textContent="Its a Draw!";
+    document.querySelector("h1").innerHTML="Its a Draw!";
     document.querySelector("h1").classList.add("smaller");
 }
